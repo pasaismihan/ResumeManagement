d476fe27295c9e4156cd2cfee5f7ae52d56608a9
pasaismihan <pasa_13142@hotmail.com>
2023-09-08 04:02:34 +0300

    Migrate Done With Dotnet CLI

---
+++ backend/Core/Entities/Job.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using backend.Core.Enums;
+
+namespace backend.Core.Entities
+{
+	public class Job : BaseEntity
+	{
+		public string Title { get; set; }
+		public JobLevel Level { get; set; }
+
+		// Relatios
+		public long CompanyId { get; set; }
+		public Company Company { get; set; }
+
+		public ICollection<Candidate> Candidates { get; set; }
+		
+	}
+}
+
