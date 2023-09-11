e1a7d137006811e19033127c18fa0867f88793fe
pasaismihan <pasa_13142@hotmail.com>
2023-09-11 23:40:14 +0300

    candidate added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,10 @@ import CustomLinearProgress from "./components/custom-linear-progress/CustomLine
 const Home = lazy(() => import("./pages/home/Home.page"));
 const Companies = lazy(() => import("./pages/companies/Companies.page"));
 const AddCompany = lazy(() => import("./pages/companies/AddCompany.page"));
+const Jobs = lazy(() => import("./pages/jobs/Jobs.page"));
+const AddJob = lazy(() => import("./pages/jobs/AddJob.page"));
+const Candidates = lazy(() => import("./pages/candidates/Candidates.pages"));
+const AddCandidate = lazy(() => import("./pages/candidates/AddCandidate.page"));
 
 const App = () => {
   const { darkMode } = useContext(ThemeContext);
@@ -23,6 +27,14 @@ const App = () => {
             <Route path="/companies">
               <Route index element={<Companies />} />
               <Route path="add" element={<AddCompany />} />
+            </Route>
+            <Route path="/jobs">
+              <Route index element={<Jobs />} />
+              <Route path="add" element={<AddJob />} />
+            </Route>
+            <Route path="/candidates">
+              <Route index element={<Candidates />} />
+              <Route path="add" element={<AddCandidate />} />
             </Route>
           </Routes>
         </Suspense>
--- frontend/src/types/global.typing.ts
+++ frontend/src/types/global.typing.ts
@@ -9,3 +9,35 @@ export interface ICreateCompanyDto {
   name: string;
   size: string;
 }
+export interface IJob {
+  id: string;
+  title: string;
+  level: string;
+  companyId: string;
+  companyName: string;
+  createdAt: string;
+}
+export interface ICreateJobDto {
+  title: string;
+  level: string;
+  companyId: string;
+}
+export interface ICandidate {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  coverLetter: string;
+  resumeUrl: string;
+  jobId: string;
+  jobTitle: string;
+}
+export interface ICreateCandidateDto {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  coverLetter: string;
+  jobId: string;
+}
